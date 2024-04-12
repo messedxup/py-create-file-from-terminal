d695491a6b69c97c7841662d200da1e536ad199d
Serhii Khodko <sergeyhodko123@gmail.com>
2024-04-12 20:18:19 +0300

    fix

--- app/create_file.py
+++ app/create_file.py
@@ -8,11 +8,15 @@ def create_directory(path: str) -> None:
 
 
 def create_file(filename: str, content: list[str]) -> None:
-    with open(filename, "a") as file:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(timestamp + "\n")
-        for idx, line in enumerate(content, start=1):
-            file.write(f"{idx} {line}\n")
+    try:
+        with open(filename, "a") as file:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(timestamp + "\n")
+            for idx, line in enumerate(content, start=1):
+                file.write(f"{idx} {line}\n")
+            file.write("\n")
+    except Exception as e:
+        print(f"Error occurred while writing to file: {e}")
 
 
 def writing_content() -> list[str]:
@@ -41,8 +45,9 @@ def main() -> None:
     create_directory(directory_path)
 
     content = writing_content()
+    content_list = [f"{line}\n" for line in content]
 
-    create_file(os.path.join(directory_path, filename), content)
+    create_file(os.path.join(directory_path, filename), content_list)
 
 
 if __name__ == "__main__":
